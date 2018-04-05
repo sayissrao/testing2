f91aa0548a22c5bc4b608336bef1efcebb6ff560
sayissrao <sayissrao@hotmail.com>
2018-04-06 03:10:33 +0800

    modified port6

--- port6.cpp
+++ port6.cpp
@@ -1,28 +1,30 @@
-#include stdafx.h
-#include iostream
-#include cmath
+#include "stdafx.h"
+#include <iostream>
+#include <cmath>
+
 
 using namespace std;
 
 int main() {
-	get input
+
 	int max = -1;
 	int counter = 0;
-	while (max  1) {
-		cout  Enter max number ;
-		cin  max;
+	while (max < 1) {
+		cout << "Enter max number: ";
+		cin >> max;
 	}
-	cout  Prime numbers up to   max  n;
-	for (int i = 1; i = max; i++) {
+
+	cout << "Prime numbers up to " << max << ":\n";
+	for (int i = 1; i <= max; i++) {
 		counter = 0;
-		for (int j = 1; j = i; j++) {
+		for (int j = 1; j <= i; j++) {
 			if (i % j == 0) {
 				counter++;
 			}
 
 		}
-		if (counter = 2) {
-			cout  i  endl;
+		if (counter <= 2) {
+			cout << i << endl;
 		}
 	}
 
